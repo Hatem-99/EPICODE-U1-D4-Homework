9a59510c79c16eea8323b9f9d6fc56e9c89a12d4
Hatem Mahmoud <hatemhindawi1999@gmail.com>
2022-07-04 14:37:47 +0200

    Homework
    java script intro

---
+++ js/index.js
@@ -0,0 +1,32 @@
+//first homework//
+let firstName = "Hatem";
+
+let lastName = "Mahmoud";
+
+let fullName = firstName + " " + lastName;
+
+console.log("hello, my name is " + fullName + ".");
+
+let age = 22;
+
+let currentYear = 2022;
+
+let birthYear = currentYear - age;
+
+console.log( birthYear );
+
+//seconed homework//
+
+"Ingredients"
+
+let spaghetti = 400;
+let guanciale = 250;
+let eggYolks = 6 ;
+let romanoCheese = 50;
+let blackPepper = 4;
+
+
+let carbonara = spaghetti + guanciale + eggYolks + romanoCheese + blackPepper ;
+
+console.log(carbonara)
+
